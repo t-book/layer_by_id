4ae68ef996e5bd0bb16920f9b88abc8a0b661ba3
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2017-06-15 13:20:26 +0200

    override for layer views.py

---
+++ layer_by_id/views.py
@@ -0,0 +1,124 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils.translation import ugettext as _
+from geonode.layers.views import *
+
+from geonode.security.views import _perms_info_json
+
+logger = logging.getLogger("geonode.layers.views")
+
+DEFAULT_SEARCH_BATCH_SIZE = 10
+MAX_SEARCH_BATCH_SIZE = 25
+GENERIC_UPLOAD_ERROR = _("There was an error while attempting to upload your data. \
+Please try again, or contact and administrator if the problem continues.")
+
+_PERMISSION_MSG_DELETE = _("You are not permitted to delete this layer")
+_PERMISSION_MSG_GENERIC = _('You do not have permissions for this layer.')
+_PERMISSION_MSG_MODIFY = _("You are not permitted to modify this layer")
+_PERMISSION_MSG_METADATA = _(
+    "You are not permitted to modify this layer's metadata")
+_PERMISSION_MSG_VIEW = _("You are not permitted to view this layer")
+
+
+
+
+def index(request,layername):
+    return HttpResponse('ok')
+
+def _resolve_layer(request, typename, permission='base.view_resourcebase',
+                   msg=_PERMISSION_MSG_GENERIC, **kwargs):
+    """
+    Resolve the layer by the provided id 
+    """
+
+    return resolve_object(request,
+                          Layer,
+                          {'id': typename,
+                          'service': None},
+                           permission=permission,
+                           permission_msg=msg,
+                           **kwargs)
+
+
+def layer_detail(request, layername, template='layers/layer_detail.html'):
+    layer = _resolve_layer(
+        request,
+        layername,
+        'base.view_resourcebase',
+        _PERMISSION_MSG_VIEW)
+
+    # assert False, str(layer_bbox)
+    config = layer.attribute_config()
+
+    # Add required parameters for GXP lazy-loading
+    layer_bbox = layer.bbox
+    bbox = [float(coord) for coord in list(layer_bbox[0:4])]
+    config["srs"] = getattr(settings, 'DEFAULT_MAP_CRS', 'EPSG:900913')
+    config["bbox"] = bbox if config["srs"] != 'EPSG:900913' \
+        else llbbox_to_mercator([float(coord) for coord in bbox])
+    config["title"] = layer.title
+    config["queryable"] = True
+
+    if layer.storeType == "remoteStore":
+        service = layer.service
+        source_params = {
+            "ptype": service.ptype,
+            "remote": True,
+            "url": service.base_url,
+            "name": service.name}
+        maplayer = GXPLayer(
+            name=layer.typename,
+            ows_url=layer.ows_url,
+            layer_params=json.dumps(config),
+            source_params=json.dumps(source_params))
+    else:
+        maplayer = GXPLayer(
+            name=layer.typename,
+            ows_url=layer.ows_url,
+            layer_params=json.dumps(config))
+
+    # Update count for popularity ranking,
+    # but do not includes admins or resource owners
+    if request.user != layer.owner and not request.user.is_superuser:
+        Layer.objects.filter(
+            id=layer.id).update(popular_count=F('popular_count') + 1)
+
+    # center/zoom don't matter; the viewer will center on the layer bounds
+    map_obj = GXPMap(projection=getattr(settings, 'DEFAULT_MAP_CRS', 'EPSG:900913'))
+
+    NON_WMS_BASE_LAYERS = [
+        la for la in default_map_config()[1] if la.ows_url is None]
+
+    metadata = layer.link_set.metadata().filter(
+        name__in=settings.DOWNLOAD_FORMATS_METADATA)
+
+    context_dict = {
+        "resource": layer,
+        'perms_list': get_perms(request.user, layer.get_self_resource()),
+        "permissions_json": _perms_info_json(layer),
+        "documents": get_related_documents(layer),
+        "metadata": metadata,
+        "is_layer": True,
+        "wps_enabled": settings.OGC_SERVER['default']['WPS_ENABLED'],
+    }
+
+    context_dict["viewer"] = json.dumps(
+        map_obj.viewer_json(request.user, * (NON_WMS_BASE_LAYERS + [maplayer])))
+    context_dict["preview"] = getattr(
+        settings,
+        'LAYER_PREVIEW_LIBRARY',
+        'leaflet')
+
+    if request.user.has_perm('download_resourcebase', layer.get_self_resource()):
+        if layer.storeType == 'dataStore':
+            links = layer.link_set.download().filter(
+                name__in=settings.DOWNLOAD_FORMATS_VECTOR)
+        else:
+            links = layer.link_set.download().filter(
+                name__in=settings.DOWNLOAD_FORMATS_RASTER)
+        context_dict["links"] = links
+
+    if settings.SOCIAL_ORIGINS:
+        context_dict["social_links"] = build_social_links(request, layer)
+
+    return render_to_response(template, RequestContext(request, context_dict))
